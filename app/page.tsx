9e7ef126e4017ca89763ed6cd187de464034d7a8
krischald-21 <krischald21@gmail.com>
2025-05-31 15:11:25 -0700

    create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div>
+      <Hero />
+    </div>
   );
 }
